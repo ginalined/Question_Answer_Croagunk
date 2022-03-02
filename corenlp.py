892ec8be7a15f4edd79f88d47195d7eb8214719e
Elina <elina@ElinadeMacBook-Pro.local>
2022-03-02 18:22:09 -0500

    add stat count

--- corenlp.py
+++ corenlp.py
@@ -4,8 +4,13 @@ if __name__ == '__main__':
     nlp = StanfordCoreNLP('http://localhost', 9000)
     sentence = 'Guangdong University of Foreign Studies is located in Guangzhou.'
     print('Tokenize: ', nlp.word_tokenize(sentence))
+    print("")
     print('Part of Speech:', nlp.pos_tag(sentence))
+    print("")
     print('Named Entities:', nlp.ner(sentence))
+    print("")
     print('Constituency Parsing: ', nlp.parse(sentence))
+    print("")
     print('Dependency Parsing:', nlp.dependency_parse(sentence))
+    print("")
     nlp.close()
