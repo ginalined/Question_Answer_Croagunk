22c42d20e97329ef51d273f049215fe2db984317
Bingxue Ouyang <oybx1428884966@gmail.com>
2022-04-08 22:26:29 -0700

    fixing stanford parser issues

--- ask.py
+++ ask.py
@@ -4,30 +4,31 @@ import nltk
 from question.question_generator import QuestionGenerator
 from nltk.parse.stanford import StanfordDependencyParser
 # https://stackoverflow.com/questions/7443330/how-do-i-do-dependency-parsing-in-nltk
-path_to_jar = 'path_to/stanford-parser-full-2020-11-17/stanford-parser.jar'
-path_to_models_jar = 'path_to/stanford-parser-full-2020-11-17/stanford-parser-4.2.0-models.jar'
+path_to_jar = 'stanford-parser-full-2020-11-17/stanford-parser.jar'
+path_to_models_jar = 'stanford-parser-full-2020-11-17/stanford-parser-4.2.0-models.jar'
 dependency_parser = StanfordDependencyParser(path_to_jar=path_to_jar, path_to_models_jar=path_to_models_jar)
 
 class Ask:
     def __init__(self, article, n):
         self.n = n
-        self.questions = dict()
+        self.questions = []
         self.processor = NLTKProcessor(article)
         self.processor.process()
     
     def generate(self):
-        self.process_ners()
+        return self.questions
 
     def process_ners(self):
         entities = [entity.text for entity in self.processor.ners.ents]
         self.entities = list(set(entities))
     
     def dep_parsing(self, sentence):
-        return dependency_parser.raw_parse(sentence)
+        result = dependency_parser.raw_parse(sentence)
+        return list(result.__next__().triples())
     
 if __name__ == '__main__':
     article = sys.argv[1]
     n = sys.argv[2]
     ask = Ask(article, n)
     ask.generate()
-    print("Entities:", ask.entities)
+    print(ask.dep_parsing("The quick brown fox jumps over the lazy dog."))
--- question/question_generator.py
+++ question/question_generator.py
@@ -1,8 +1,9 @@
-from wh_question import WhQuestion
-from yes_no_question import YesNoQuestion
+from question.wh_question import WhQuestion
+from question.yes_no_question import YesNoQuestion
 from nltk.parse.corenlp import CoreNLPDependencyParser
 dep_parser = CoreNLPDependencyParser(url='http://localhost:9000')
 
 class QuestionGenerator:
     def __init__(self):
         return
+        
