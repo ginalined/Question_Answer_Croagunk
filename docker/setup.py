c34c6158907c12a9cfd6dc1503db19fb45c624ae
Bingxue Ouyang <oybx1428884966@gmail.com>
2022-04-13 23:55:08 -0700

    adding punkt for nltk

--- docker/setup.py
+++ docker/setup.py
@@ -3,6 +3,7 @@ import stanza
 
 stanza.download('en')
 nltk.download('wordnet', quiet=True)
+nltk.download('punkt', quiet=True)
 nltk.download('words', quiet=True)
 nltk.download('wordnet', quiet=True)
 nltk.download('omw-1.4', quiet=True)
