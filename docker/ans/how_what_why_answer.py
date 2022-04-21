7f53e97fa23a989f3cda7d401a63fb81e142e4a3
SimbeImyne <haogao@andrew.cmu.edu>
2022-04-21 17:41:01 -0400

    fix yesno

--- docker/ans/how_what_why_answer.py
+++ docker/ans/how_what_why_answer.py
@@ -24,37 +24,7 @@ class HowWhatWhyAnswer:
                 return ent.text
         return "why can't find answer"
 
-    def vector_sentence (self, term_f):
-        word_f = {}
-        vector_question = {}
-        for word in term_f:
-            vector_question[word] = term_f[word] * word_f.get(word, 0)
-        return vector_question;
 
-    def rank(self):
-        best_id = 0;
-        best_similar = -1
-        lemmas = []
-        for token in self.sentence.ents:
-            lemmas.append(token.lemma_)
-        term_f = {}
-        for word in lemmas:
-            term_f[word] = term_f.get(word, 0) + 1
-        vector_question = self.vector_sentence(term_f)
-        sentences = []
-        for sentence_id in range(len(sentences)):
-            similarity = 0
-            qvalue = 0
-            svalue = 0
-            for vq in vector_question:
-                if vq in sentences[sentence_id]:
-                    similarity += vector_question * sentences[sentence_id]
-                    qvalue += vector_question ** 2
-                    svalue += sentences[sentence_id] ** 2
-                    if similarity > best_similarity:
-                        best_similarity = similarity
-                        best_idx = sentence_id
-        return sentences[best_idx]
 
 
 
